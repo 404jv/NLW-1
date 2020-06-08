ddf47038dcfd4abfa94a73e5dd0aa6f08b9ec75c
João Victor Ramalho <joaovictorramalho7@gmail.com>
2020-06-07 21:39:45 -0300

    funcionalidades avançadas

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,15 +1,33 @@
 import { Router } from 'express';
+import multer from 'multer';
+import { celebrate, Joi } from 'celebrate';
+import multerConfig from './config/multer';
 
 import PointsController from './controllers/PointsController';
 import ItemsController from './controllers/ItemsController';
 
 const routes = Router();
+const upload = multer(multerConfig);
+
 const pointsConstroller = new PointsController();
 const itemsController = new ItemsController();
 
 routes.get('/items', itemsController.index);
 
-routes.post('/points', pointsConstroller.create);
+routes.post('/points', upload.single('image'), celebrate({
+  body: Joi.object().keys({
+    name: Joi.string().required(),
+    email: Joi.string().required().email(),
+    whatsapp: Joi.string().required(),
+    latitude: Joi.number().required(),
+    longitude: Joi.number().required(),
+    city: Joi.string().required(),
+    uf: Joi.string().required().max(2),
+    items: Joi.string().required(),
+  }),
+}, {
+  abortEarly: false
+}), pointsConstroller.create);
 routes.get('/points', pointsConstroller.index);
 routes.get('/points/:id', pointsConstroller.show);
 
--- server/src/server.ts
+++ server/src/server.ts
@@ -2,6 +2,7 @@ import express from 'express';
 import routes from './routes';
 import cors from 'cors';
 import path from 'path'
+import { errors } from 'celebrate';
 
 const app = express();
 app.use(cors());
@@ -9,5 +10,7 @@ app.use(express.json());
 app.use(routes);
 
 app.use('/uploads', express.static(path.resolve(__dirname, '..', 'uploads')));
+
+app.use(errors);
 
 app.listen(3333, () => console.log('Server is runnig: http://localhost:3333'));
